2823c763931774d7cb58e6afae6917ff762fa9e3
Adam Fields <afields@6river.com>
2023-04-24 01:27:34 -0400

    feat: show email and key icon in fish prompt

--- mac/.config/fish/functions/fish_prompt.fish
+++ mac/.config/fish/functions/fish_prompt.fish
@@ -1,30 +1,46 @@
 # Fish prompt by @adamelliotfields 🐟
-# See /usr/local/share/fish/functions/fish_prompt.fish for the default prompt
+# See $HOMEBREW_PREFIX/share/fish/functions/fish_prompt.fish for the default prompt
 function fish_prompt --description 'Write out the prompt'
-  set -lx __fish_last_status $status # export for __fish_print_pipestatus
+  # icons
+  set -l nf_fa_key \uf084           # 
+  set -l nf_fa_chevron_right \uf054 # 
+  set -l nf_fa_hashtag \uf292       # 
+
+  # exit status including pipe status
+  set -lx __fish_last_status $status # local export for __fish_print_pipestatus
   set -l __fish_last_status_error 0
   set -l __last_pipestatus $pipestatus
-  set -l __fish_prompt_char \uf054 #  nf-fa-chevron_right
   set -q __fish_prompt_status_generation
     or set -g __fish_prompt_status_generation $status_generation
 
-  # Show user@host only in a SSH session
-  # User color: $fish_color_user
-  # Host color: $fish_color_host_remote
+  # show user@host only in a SSH session
+  #   - user: $fish_color_user
+  #   - host: $fish_color_host_remote
   if set -q SSH_TTY
     printf '%s%s' (prompt_login) ' '
   end
 
-  # PWD
-  printf '%s%s%s' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  # pwd
+  printf '%s%s%s%s' (set_color $fish_color_cwd) (prompt_pwd) ' ' (set_color normal)
 
-  # Git
-  # See https://fishshell.com/docs/current/cmds/fish_git_prompt.html
+  # git signing key
+  set -l signingkey (git config --global user.signingkey 2>/dev/null)
+  if test -n "$signingkey"
+    printf '%s%b%s%s' (set_color yellow) $nf_fa_key ' ' (set_color normal)
+  end
+
+  # git email
+  set -l email (git config --global user.email 2>/dev/null)
+  if test -n "$email"
+    printf '%s%s%s' (set_color cyan) $email (set_color normal)
+  end
+
+  # git status
+  # https://fishshell.com/docs/current/cmds/fish_git_prompt.html
   printf '%s%s%s' (fish_git_prompt) ' ' (set_color normal)
 
-  # Status
-  # The `$status_generation` variable increments when the previous command returned a status code.
-  if [ $__fish_prompt_status_generation != $status_generation ]
+  # $status_generation increments when the previous command returned a status code
+  if test $__fish_prompt_status_generation -ne $status_generation
     __fish_print_pipestatus '[' '] ' '|' (set_color $fish_color_normal) (set_color $fish_color_error) $__last_pipestatus
     set -g __fish_prompt_status_generation $status_generation
     if not contains $__fish_last_status 0 141
@@ -32,17 +48,20 @@ function fish_prompt --description 'Write out the prompt'
     end
   end
 
-  # Command duration (requires jorgebucaran/humantime.fish)
+  # command duration (requires jorgebucaran/humantime.fish)
   if type -q humantime
     printf '%s%s%s%s' (set_color $fish_color_autosuggestion) (humantime $CMD_DURATION) ' ' (set_color normal)
   end
 
-  # Newline prompt character
+  # set prompt character color
   test $__fish_last_status_error -eq 1
     and set_color $fish_color_error
     or set_color $fish_color_normal
+
+  # set prompt character
+  set -l __fish_prompt_char $nf_fa_chevron_right
   fish_is_root_user
-    and set __fish_prompt_char \uf292 #  nf-fa-hashtag
-  printf '%b%b%s' '\n' $__fish_prompt_char ' '
-  set_color normal
+    and set __fish_prompt_char $nf_fa_hashtag
+
+  printf '%b%b%s' '\n' $__fish_prompt_char ' ' (set_color normal)
 end
