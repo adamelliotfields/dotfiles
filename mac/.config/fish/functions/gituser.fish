7c6b8f939d10faed4daca1f614ed5987bfe8c964
Adam Fields <adam.elliot.fields@gmail.com>
2023-05-02 21:24:09 -0400

    feat: add completions

--- mac/.config/fish/functions/gituser.fish
+++ mac/.config/fish/functions/gituser.fish
@@ -1,5 +1,5 @@
 # Need `gpg` and optionally `bat`:
-#   - Linux: `apt install gnpupg dirmngr bat`
+#   - Linux: `apt install gnpupg bat`
 #   - macOS: `brew install gnupg bat`
 #
 # Your *secret* key must be in your GPG keyring.
@@ -24,36 +24,75 @@
 # gh gpg-key add /tmp/pub.asc # requires gpg scope on your GH_TOKEN
 # ```
 function gituser -d 'Switches the current Git user'
-  argparse 'd/dry-run' 'e/email' 'h/help' 'k/key' 'x/xdg' -- $argv ; or return 1
+  set -l completions_txt 'Print Fish completions'
+  set -l dry_run_txt 'Print the git config changes without making them'
+  set -l email_txt 'Print the current email address'
+  set -l help_txt 'Print this help message and exit'
+  set -l key_txt 'Print the current signing key'
+  set -l list_txt 'List all of the email addresses in your keyring'
+  set -l quiet_txt 'Suppress all output'
+  set -l xdg_txt 'Use $XDG_CONFIG_HOME/git/config instead of default git config handling'
 
+  set -l help_msg "gituser\n
+Updates your Git and GPG configs to use the given email address and associated key.
+Requires the key to be in your keyring. Will clobber your existing GPG config if you have one.\n
+EXAMPLES:
+  gituser email@example.com
+  gituser (gituser -l | fzf)\n
+USAGE:
+  gituser [USER] [FLAGS]\n
+USER:
+  The email address to use for the Git user.\n
+FLAGS:
+      --completions    $completions_txt
+  -d, --dry-run        $dry_run_txt
+  -e, --email          $email_txt
+  -h, --help           $help_txt
+  -k, --key            $key_txt
+  -l, --list           $list_txt
+  -q, --quiet          $quiet_txt
+  -x, --xdg            $xdg_txt"
+
+  argparse 'completions' 'd/dry-run' 'e/email' 'h/help' 'k/key' 'l/list' 'q/quiet' 'x/xdg' -- $argv ; or return 1
+
+  set -l completions $_flag_completions
   set -l dry_run $_flag_d
   set -l email $_flag_e
-  set -l help $_flag_h
+  set -l _help $_flag_h
   set -l key $_flag_k
+  set -l list $_flag_l
+  set -l quiet $_flag_q
   set -l xdg $_flag_x
 
   set -l user $argv
-  set -l help_msg "gituser
-
-Updates your Git and GPG configs to use the given email address and associated key.
-Requires the key to be in your keyring. Will clobber your existing GPG config if you have one.
-
-USAGE:
-  gituser [ARGS] [FLAGS]
-
-ARGS:
-  user           The email address of the user to sign commits
-
-FLAGS:
-  -d, --dry-run  Print the git config changes without making them
-  -e, --email    Print the current email address
-  -h, --help     Print this help message and exit
-  -k, --key      Print the current signing key
-  -x, --xdg      Use \$XDG_CONFIG_HOME/git/config instead of default git config handling"
 
   # exit early for help
-  if test -n "$help"
-    echo $help_msg
+  # overrides all other flags
+  if test -n "$_help"
+    echo -e $help_msg
+    return 0
+  end
+
+  # print completions
+  # usage: gituser --completions | source
+  if test -n "$completions"
+    echo "\
+complete -c gituser -f
+complete -c gituser -a '(gituser --list)'
+complete -c gituser -l completions -d '$completions_txt'
+complete -c gituser -s d -l dry-run -d '$dry_run_txt'
+complete -c gituser -s e -l email -d '$email_txt'
+complete -c gituser -s h -l help -d '$help_txt'
+complete -c gituser -s k -l key -d '$key_txt'
+complete -c gituser -s l -l list -d '$list_txt'
+complete -c gituser -s q -l quiet -d '$quiet_txt'
+complete -c gituser -s x -l xdg -d '$xdg_txt'"
+    return 0
+  end
+
+  # list email addresses
+  if test -n "$list"
+    test -z "$quiet" ; and gpg --list-keys | awk -F'[<>]' '/</ { print $2 }'
     return 0
   end
 
@@ -88,8 +127,8 @@ FLAGS:
 
   # print the current user or key and exit if one of the flags was passed
   if test -n "$email" ; or test -n "$key"
-    test -n "$email" ; and echo $current_user
-    test -n "$key" ; and echo $current_key_id
+    test -z "$quiet" -a -n "$email" ; and echo $current_user
+    test -z "$quiet" -a -n "$key" ; and echo $current_key_id
     return 0
   end
 
@@ -100,7 +139,7 @@ FLAGS:
   signingkey = $current_key_id"
 
   if test -z "$user" ; or test "$user" = "$current_user"
-    echo $result
+    test -z "$quiet" ; and echo $result
     return 0
   end
 
@@ -132,17 +171,19 @@ FLAGS:
     echo "default-key $key_id" | tee ~/.gnupg/gpg.conf >/dev/null
   end
 
-  # show a "diff" of the changes
-  set -l diff "[user]
+  # print a color diff if bat is installed
+  if test -z "$quiet"
+    # show a "diff" of the changes
+    set -l diff "[user]
 +  email = $user
 -  email = $current_user
 +  signingkey = $key_id
 -  signingkey = $current_key_id"
 
-  # print a color diff if bat is installed
-  if command -v bat >/dev/null
-    echo $diff | bat -pp -l diff --color=always
-  else
-    echo $diff
+    if command -v bat >/dev/null
+      echo $diff | bat -pp -l diff --color=always
+    else
+      echo $diff
+    end
   end
 end
