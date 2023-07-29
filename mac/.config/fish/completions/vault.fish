87a15ff5c9ae456ac8bae5e5743053869d620387
Adam Fields <adam.elliot.fields@gmail.com>
2023-07-29 10:13:16 -0400

    add or remove programs

--- mac/.config/fish/completions/vault.fish
+++
@@ -1,8 +0,0 @@
-function __complete_vault
-  set -lx COMP_LINE (commandline -cp)
-  test -z (commandline -ct) ; and set COMP_LINE $COMP_LINE' '
-  vault
-end
-
-complete -c vault -f
-complete -c vault -a "(__complete_vault)"
