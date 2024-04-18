faceebe07c50771967218fda7ffba0601ef4bb26
Adam Fields <me@aef.me>
2024-04-18 15:57:33 -0400

    Update abbreviations

--- shared/.config/fish/conf.d/abbreviations.fish
+++ shared/.config/fish/conf.d/abbreviations.fish
@@ -1,18 +1,16 @@
-# abbreviations
-# like aliases except they are expanded on `space`
+# abbreviations are like aliases except they are expanded on `space`
 # using them to keep mac-only aliases separate from shared
 # https://fishshell.com/docs/current/cmds/abbr.html
 abbr -a -- refresh exec fish
 abbr -a -- restart sudo shutdown -r now
 abbr -a -- shutdown sudo shutdown -h now
+abbr -a -- update 'brew update && brew upgrade && brew cleanup'
 abbr -a -- mute "osascript -e 'set volume output muted true'"
 abbr -a -- unmute "osascript -e 'set volume output muted false'"
 abbr -a -- lock "osascript -e 'tell application \"System Events\" to keystroke \"q\" using {control down, command down}'"
 abbr -a -- logout "osascript -e 'tell application \"System Events\" to log out'"
 abbr -a -- finder "osascript -e 'tell application \"Finder\" to activate' -e 'tell application \"Finder\" to open home'"
 abbr -a -- trash "osascript -e 'tell application \"Finder\" to empty trash'"
-abbr -a -- update 'brew update && brew upgrade && brew cleanup'
-abbr -a -- octal "stat -f '%A'" # use `%a` on linux
-abbr -a -- uuid "uuidgen | tr '[:upper:]' '[:lower:]'"
-abbr -a -- todo "rg 'TODO: |FIXME: '"
+
+# interactively switch git user
 abbr -a -- fzgu 'gituser (gituser --list | fzf)'
