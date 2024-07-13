5fb2fed2ef75a884927e478283080a262ba4863a
Adam Fields <me@aef.me>
2024-07-13 16:18:16 +0000

    Fix missing GH_TOKEN

--- lib/deb.sh
+++ lib/deb.sh
@@ -2,32 +2,37 @@
 # downloads a list of deb packages from GitHub releases and installs them with dpkg (linux only)
 function dotfiles_deb {
   source "$(dirname "${BASH_SOURCE[0]}")/sudo.sh"
-
   local -a repos=("$@")
+
+  # delay between requests when unauthenticated
+  local DELAY=1
+
+  # prefer GH_TOKEN
   local token="${GITHUB_TOKEN:-}"
   local token="${GH_TOKEN:-$token}"
 
   if [[ -z $(command -v dpkg 2>/dev/null) || "$(uname -s)" != 'Linux' ]] ; then
-    echo "dotfiles_deb: Unsupported OS"
+    echo 'dotfiles_deb: Unsupported OS'
     return 1
-  fi
-
-  # don't panic if token isn't set
-  if [[ -z "$token" ]] ; then
-    echo "dotfiles_deb: GH_TOKEN not set... skipping"
-    return 0
   fi
 
   for repo in "${repos[@]}" ; do
     local bin=$(echo "$repo" | awk -F '/' '{print $2}')
-    local opts="-fsSLH 'Authorization: token $token'"
+    local opts='-fsSL'
     local arch=''
+
+    # if token is available use it
+    if [[ -n "$token" ]] ; then
+      echo 'dotfiles_deb: Using GitHub API token...'
+      opts="$opts -H 'Authorization: token $token'"
+      DELAY=0
+    fi
 
     # set arch
     case "$(uname -m)" in
       aarch64|arm64) arch='arm64' ;;
       x86_64|amd64)  arch='amd64' ;;
-      *)             echo "dotfiles_deb: Unsupported architecture" ; return 1 ;;
+      *)             echo 'dotfiles_deb: Unsupported architecture' ; return 1 ;;
     esac
 
     # GitHub CLI is installed as `gh`
@@ -54,7 +59,7 @@ function dotfiles_deb {
     # download the deb
     local browser_download_url=$(echo "$asset" | jq -r '.browser_download_url')
     local filename=$(basename "$browser_download_url")
-    eval "curl -fsSL $browser_download_url -o /tmp/$filename"
+    sleep $DELAY && eval "curl -fsSL $browser_download_url -o /tmp/$filename"
 
     # install and cleanup
     dotfiles_sudo dpkg -i "/tmp/$filename"
