46aff525f42805667a09f9ea42db4d4fc15edb1e
Adam Fields <me@aef.me>
2024-11-20 10:18:00 -0500

    Don't install bun completions post install

--- lib/bun.sh
+++ lib/bun.sh
@@ -4,15 +4,14 @@ dotfiles_bun() {
   source "$(dirname "${BASH_SOURCE[0]}")/sudo.sh"
 
   local url='https://github.com/oven-sh/bun/releases/latest/download'
-  local bash_completion_dir=''
   local target=''
 
   # set target
   case "$(uname -ms)" in
-    'Darwin x86_64')                 target='darwin-x64' ; bash_completion_dir="${HOMEBREW_PREFIX:-/usr/local}/etc/bash_completion.d" ;;
-    'Darwin arm64')                  target='darwin-aarch64' ; bash_completion_dir="${HOMEBREW_PREFIX:-/opt/homebrew}/etc/bash_completion.d" ;;
-    'Linux x86_64')                  target='linux-x64' ; bash_completion_dir='/etc/bash_completion.d' ;;
-    'Linux aarch64' | 'Linux arm64') target='linux-aarch64' ; bash_completion_dir='/etc/bash_completion.d' ;;
+    'Darwin x86_64')                 target='darwin-x64' ;;
+    'Darwin arm64')                  target='darwin-aarch64' ;;
+    'Linux x86_64')                  target='linux-x64' ;;
+    'Linux aarch64' | 'Linux arm64') target='linux-aarch64' ;;
     *)                               echo 'dotfiles_bun: Unsupported target' ; return 1 ;;
   esac
 
@@ -49,12 +48,6 @@ dotfiles_bun() {
   mkdir -p "$bun_bin_dir"
   mv "/tmp/bun-${target}/bun" "$bun_bin_dir"
   chmod +x "${bun_bin_dir}/bun"
-
-  # install completions
-  # also installs bunx
-  # https://github.com/oven-sh/bun/blob/main/src/cli/install_completions_command.zig
-  dotfiles_sudo rm -f "${bash_completion_dir}/bun"
-  "${bun_dir}/bin/bun" completions | dotfiles_sudo tee "${bash_completion_dir}/bun" >/dev/null
 
   # cleanup
   rm -rf "/tmp/bun-${target}"
