d27a3f37ca7abaa39dff1e2271aa5841d417392f
Adam Fields <me@aef.me>
2025-06-03 21:56:43 -0400

    Add PowerShell config

--- install.ps1
+++ install.ps1
@@ -3,3 +3,6 @@ New-Item -Force -ItemType directory (Split-Path $Profile)
 
 # Symlink profile
 New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot/windows/profile.ps1" -Path $Profile
+
+# Symlink config
+New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot/windows/config.json" -Path "$Home\Documents\PowerShell\powershell.config.json"
