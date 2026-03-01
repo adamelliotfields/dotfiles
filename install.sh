6e06760b28bcf360686193817c58e0205218a347
Adam Fields <adam.elliot.fields@gmail.com>
2026-03-01 12:24:59 -0500

    Remove yarn before installing other packages on Linux

--- install.sh
+++ install.sh
@@ -14,11 +14,22 @@ declare -a linux_bin=( 'fzf' 'gh' 'lf' 'micro' )
 echo 'Symlinking dotfiles...'
 dotfiles_link
 
+function _sudo {
+  [[ $EUID -ne 0 ]] && sudo "$@" || "$@"
+}
+
 # Omit Python, Node, and Fish.
 # This is mostly run in dev containers which already have runtimes installed.
 # Installing Fish requires a lot of dependencies which slows down container creation.
 if [[ $(uname -s) == 'Linux' ]] ; then
   export DEBIAN_FRONTEND=noninteractive
+
+  # remove yarn if present
+  if command -v yarn >/dev/null 2>&1 ; then
+    echo 'Removing yarn...'
+    _sudo apt remove -y --purge yarn
+    _sudo rm -f /etc/apt/sources.list.d/yarn.list
+  fi
 
   echo 'Installing apt packages...'
   dotfiles_apt "${linux_apt[@]}" "${linux_apt_python[@]}"
