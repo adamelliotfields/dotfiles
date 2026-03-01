37e991f8dcb79753b23aad1af7b46aa8cf22de6d
Adam Fields <adam.elliot.fields@gmail.com>
2026-03-01 12:01:19 -0500

    Delete

--- lib/sudo.sh
+++
@@ -1,8 +0,0 @@
-# escalates privileges if necessary
-function dotfiles_sudo {
-  if [[ $EUID -ne 0 ]] ; then
-    sudo "$@"
-  else
-    "$@"
-  fi
-}
--- shared/.config/.hyper_plugins/local/hyper-quit/index.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/cblecker/hyper-quit (Apache 2.0)
-const { app } = require('electron')
-
-exports.onWindow = (w) => {
-  w.on('closed', () => {
-    const { size } = app.getWindows()
-    if (size === 0) app.quit()
-  })
-}
