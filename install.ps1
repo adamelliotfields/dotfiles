33e0d2ce9c738e9e8fe59dbb8bbea2b0dcb58708
Adam Fields <adam.elliot.fields@gmail.com>
2026-03-01 11:18:14 -0500

    Update install scripts

--- install.ps1
+++ install.ps1
@@ -1,8 +1,6 @@
-# Ensure profile directory exists
-New-Item -Force -ItemType directory (Split-Path $Profile)
+# Ensure PowerShell profile directory exists
+New-Item -Force -ItemType directory (Split-Path $PROFILE)
 
-# Symlink profile
-New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot/windows/profile.ps1" -Path $Profile
-
-# Symlink config
-New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot/windows/config.json" -Path "$Home\Documents\PowerShell\powershell.config.json"
+# Symlink profile and config
+New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot\windows\profile.ps1" -Path $PROFILE
+New-Item -Force -ItemType SymbolicLink -Target "$PSScriptRoot\windows\config.json" -Path "$HOME\Documents\PowerShell\powershell.config.json"
--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for func in 'apt' 'deb' 'homebrew' 'link' ; do
+for func in 'apt' 'bin' 'deb' 'homebrew' 'link' ; do
   source "$(dirname "${BASH_SOURCE[0]}")/lib/${func}.sh"
 done
 
-declare -a linux_apt=( 'aria2' 'build-essential' 'curl' 'fzf' 'git' 'git-lfs' 'gnupg' 'jq' 'libfuse2' 'nano' 'ripgrep' 'unzip' 'wget' )
+declare -a linux_apt=( 'aria2' 'build-essential' 'curl' 'file' 'git' 'git-lfs' 'gnupg' 'jq' 'libfuse2' 'sqlite3' 'wget' 'xsel' )
 declare -a linux_apt_python=( 'libbz2-dev' 'libffi-dev' 'liblzma-dev' 'libncurses-dev' 'libreadline-dev' 'libsqlite3-dev' 'libssl-dev' 'zlib1g-dev' )
-declare -a linux_deb=( 'cli/cli' 'lsd-rs/lsd' 'sharkdp/bat' 'sharkdp/diskus' 'sharkdp/fd' 'ajeetdsouza/zoxide' )
+
+declare -a linux_deb=( 'burntsushi/ripgrep' 'lsd-rs/lsd' 'sharkdp/fd' 'ajeetdsouza/zoxide' )
+declare -a linux_bin=( 'fzf' 'gh' 'lf' 'micro' )
 
 echo 'Symlinking dotfiles...'
 dotfiles_link
 
-# For Linux, omit Python, Node, and Fish.
+# Omit Python, Node, and Fish.
 # This is mostly run in dev containers which already have runtimes installed.
 # Installing Fish requires a lot of dependencies which slows down container creation.
 if [[ $(uname -s) == 'Linux' ]] ; then
@@ -24,28 +26,22 @@ if [[ $(uname -s) == 'Linux' ]] ; then
   echo 'Installing deb packages...'
   dotfiles_deb "${linux_deb[@]}"
 
-  echo 'Installing liquidprompt...'
-  rm -rf "${HOME:?}/.liquidprompt"
-  git clone --depth=1 --branch=stable https://github.com/nojhan/liquidprompt.git "${HOME:?}/.liquidprompt"
+  echo 'Installing binaries...'
+  dotfiles_bin "${linux_bin[@]}"
 
   unset DEBIAN_FRONTEND
 fi
 
-# For Mac, run `xcode-select --install` to install developer tools.
-# Then, run `dotfiles_sudoers` and logout to grant passwordless sudo to the current user.
+# Run `xcode-select --install` to install developer tools.
 if [[ $(uname -s) == 'Darwin' ]] ; then
   echo 'Installing Homebrew...'
   dotfiles_homebrew
 
   echo 'Installing Homebrew packages...'
   eval "$(brew shellenv)"
-  brew bundle --global --no-lock
+  brew install fish fisher node python
 
-  # requires fnm (installed by brew)
-  echo 'Installing Node.js LTS...'
-  fnm install 'lts/*'
-
-  # requires fish and fisher (installed by brew)
+  # requires fish and fisher (installed above)
   echo 'Installing Fish plugins...'
   fish -c 'fisher update'
 
